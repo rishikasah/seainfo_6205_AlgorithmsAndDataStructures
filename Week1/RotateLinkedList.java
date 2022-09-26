a77b0c795bf4d372db52a33c7129740662ede878
rishikasah <98227949+rishikasah@users.noreply.github.com>
2022-09-26 15:08:32 -0700

    Adding question

--- Week1/RotateLinkedList.java
+++ Week1/RotateLinkedList.java
@@ -1,4 +1,11 @@
 package Week1;
+/*
+ * Given the head of a linked list, rotate the list to the right by k places.
+ * CONSTRAINTS: 
+ * The number of nodes in the list is in the range [0, 500].
+ * -100 <= Node.val <= 100
+ * 0 <= k <= 2 * 109
+ */
 
 public class RotateLinkedList {
 
