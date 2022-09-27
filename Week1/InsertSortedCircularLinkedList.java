8e34295a1639a30e8293cef40cd1cbcea8ae1272
rishikasah <98227949+rishikasah@users.noreply.github.com>
2022-09-26 23:57:28 -0700

    Update question

--- Week1/InsertSortedCircularLinkedList.java
+++ Week1/InsertSortedCircularLinkedList.java
@@ -1,5 +1,13 @@
 package Week1;
 
+/*
+ * Given a Circular Linked List node, which is sorted in non-descending order, write a function to insert a value insertVal into the list such that it remains a sorted circular list. 
+ * The given node can be a reference to any single node in the list and may not necessarily be the smallest value in the circular list.
+ * If there are multiple suitable places for insertion, you may choose any place to insert the new value. 
+ * After the insertion, the circular list should remain sorted.
+ * If the list is empty (i.e., the given node is null), you should create a new single circular list and return the reference to that single node. 
+ * Otherwise, you should return the originally given node.
+ */
 public class InsertSortedCircularLinkedList {
 
     public InsertSortedCircularLinkedList() {
@@ -14,7 +22,7 @@ public class InsertSortedCircularLinkedList {
          *   3. insertVal super big or insertVal super small, insert into the fall down position.
          * Corner case, head == null.
          */
-        
+
         if(head == null){
             head = new ListNode(insertVal, null);
             head.next = head;
