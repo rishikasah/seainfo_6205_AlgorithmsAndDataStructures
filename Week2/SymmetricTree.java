208d69f433394445d140fffb2a103670d3853022
rishikasah <98227949+rishikasah@users.noreply.github.com>
2022-10-11 16:37:14 -0700

    Symmetric tree submission.

--- Week2/SymmetricTree.java
+++ Week2/SymmetricTree.java
@@ -1,5 +1,22 @@
 package Week2;
-
+//Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
 public class SymmetricTree {
+  
+  public boolean isSymmetric(TreeNode root) {
+        return root == null || isMirror(root.left, root.right);
+    }
+
+    boolean isMirror(TreeNode nodeLeft, TreeNode nodeRight) {
+        if (nodeLeft == null && nodeRight == null)
+            return true;
+        
+        if (nodeLeft == null || nodeRight == null) 
+            return false;
+        
+        if (nodeLeft.val != nodeRight.val) 
+            return false;
+
+        return isMirror(nodeLeft.left, nodeRight.right) && isMirror(nodeLeft.right, nodeRight.left);
+    }
   
 }
