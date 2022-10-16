6582641351add68cde47b1103b4a02f65d412425
rishikasah <98227949+rishikasah@users.noreply.github.com>
2022-10-16 10:26:27 -0700

    Same Tree and Rotate Array

---
+++ quiz1/SameTree.java
@@ -0,0 +1,17 @@
+class SameTree {
+
+  public boolean isSameTree(TreeNode p, TreeNode q) {
+
+    if (p == null && q == null)
+      return true;
+
+    // one of p and q is null
+    if (q == null || p == null)
+      return false;
+
+    if (p.val != q.val)
+      return false;
+
+    return isSameTree(p.right, q.right) && isSameTree(p.left, q.left);
+  }
+}
