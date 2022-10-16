b7a4f9915c83bc905ad02763364d18ee754ad2ad
rishikasah <98227949+rishikasah@users.noreply.github.com>
2022-10-16 10:30:27 -0700

    Level Order Traversal B Tree

---
+++ quiz1/BTreeLevelOrderTraversal.java
@@ -0,0 +1,25 @@
+class BTreeLevelOrderTraversal {
+
+    List<List<Integer>> levels = new ArrayList<List<Integer>>();
+
+    public void BTreeHelper(TreeNode node, int level) {
+        // start the current level
+        if (levels.size() == level)
+            levels.add(new ArrayList<Integer>());
+
+         // fulfil the current level
+         levels.get(level).add(node.val);
+
+         // process child nodes for the next level
+         if (node.left != null)
+            BTreeHelper(node.left, level + 1);
+         if (node.right != null)
+            BTreeHelper(node.right, level + 1);
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if (root == null) return levels;
+        BTreeHelper(root, 0);
+        return levels;
+    }
+}
